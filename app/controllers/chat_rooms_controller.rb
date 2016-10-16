ebc747c6ca3ca9695b9c42ec9d7a1e6d8bd2590b
Bhaskar <bkotu6717@gmail.com>
2016-10-16 12:00:32 +0530

    Finish server side scripting for action cable

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,25 +1,27 @@
 class ChatRoomsController < ApplicationController
 	def index
-    @chat_rooms = ChatRoom.all
-  end
+    	@chat_rooms = ChatRoom.all
+  	end
 
-  def new
-    @chat_room = ChatRoom.new
-  end
+  	def new
+    	@chat_room = ChatRoom.new
+  	end
 
-  def show
-  	@chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+  	def show
+  		@chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+  		@message = Message.new
 	end
 
-  def create
-    @chat_room = current_user.chat_rooms.build(chat_room_params)
-    if @chat_room.save
-      flash[:success] = 'Chat room added!'
-      redirect_to chat_rooms_path
-    else
-      render 'new'
-    end
-  end
+  
+  	def create
+    	@chat_room = current_user.chat_rooms.build(chat_room_params)
+    	if @chat_room.save
+      		flash[:success] = 'Chat room added!'
+      		redirect_to chat_rooms_path
+    	else
+      		render 'new'
+    	end
+  	end
 
   private
 
